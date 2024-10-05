95e18b9e39d1d5a735e48b2b479bf4516ba062da
Khatana-AZM <waqar.khatana@azm.dev>
2024-10-05 16:45:09 +0500

    initial setup and logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import countries from './countries';
 
 function App() {
+  const [currentCountry, setCurrentCountry] = useState(0);
+  const [userGuess, setUserGuess] = useState('');
+  const [revealedClues, setRevealedClues] = useState([0]);
+  const [tries, setTries] = useState(0);
+  const [message, setMessage] = useState('');
+
+  const handleGuess = () => {
+    if (userGuess.toLowerCase() === countries[currentCountry].name.toLowerCase()) {
+      setMessage('Correct');
+    } else {
+        if (tries < 4) {
+          setRevealedClues([...revealedClues, revealedClues.length]);
+          setTries(tries + 1);
+          setMessage('Wrong! Try again.');
+        } else {
+          setMessage(`Sorry! The correct answer was ${countries[currentCountry].name}.`);
+        }
+    }
+    setUserGuess(''); 
+  };
+
+  const nextCountry = () => {
+    if (currentCountry < countries.length - 1) {
+      setCurrentCountry(currentCountry + 1);
+    } else {
+      setCurrentCountry(0);
+    }
+    setRevealedClues([0]);
+    setTries(0);
+    setMessage('');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Guess the Country</h1>
+      <div>
+        <ul>
+          {revealedClues.map((clueIndex) => (
+            <li key={clueIndex}>{countries[currentCountry].clues[clueIndex]}</li>
+          ))}
+        </ul>
+      </div>
+
+      <input
+        type="text"
+        value={userGuess}
+        onChange={(e) => setUserGuess(e.target.value)}
+        placeholder="Enter your guess"
+      />
+      <button onClick={handleGuess}>Submit Guess</button>
+
+      <p>{message}</p>
     </div>
   );
 }
---
+++ src/countries.js
@@ -0,0 +1,24 @@
+const countries = [
+    {
+        name: "Pakistan",
+        clues: [
+            'hello',
+            'by',
+            'hello',
+            'again',
+            'by'
+        ]
+    },
+    {
+        name: "Afghanistan",
+        clues: [
+            'hello',
+            'by',
+            'hello',
+            'again',
+            'by'
+        ]
+    }
+]
+
+export default countries;
