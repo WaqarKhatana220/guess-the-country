d28c576bf5af2e44f91025b7a2e4003fc64cd383
Khatana-AZM <waqar.khatana@azm.dev>
2024-10-07 01:51:53 +0500

    current working game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import countries from './countries';
+import Modal from './Modal'; // Import the Modal component
+import logo from './logo.png';
 
 function App() {
   const [currentCountry, setCurrentCountry] = useState(0);
@@ -9,11 +11,16 @@ function App() {
   const [tries, setTries] = useState(0);
   const [message, setMessage] = useState('');
   const [answerRevealed, setAnswerRevealed] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false); // State for modal visibility
 
   const handleGuess = () => {
+    // if (userGuess.length === 0) {
+    //   return;
+    // }
     if (userGuess.toLowerCase() === countries[currentCountry].name.toLowerCase()) {
       setMessage('Correct');
       setAnswerRevealed(true); // Reveal the answer when correct
+      setModalVisible(true); // Show the modal
     } else {
       if (tries < 4) {
         setRevealedClues([...revealedClues, revealedClues.length]);
@@ -21,9 +28,10 @@ function App() {
       } else {
         setMessage(countries[currentCountry].name);
         setAnswerRevealed(true); // Reveal the answer when tries are over
+        setModalVisible(true); // Show the modal
       }
     }
-    setUserGuess(''); 
+    setUserGuess('');
   };
 
   const nextCountry = () => {
@@ -36,6 +44,7 @@ function App() {
     setTries(0);
     setMessage('');
     setAnswerRevealed(false); // Reset answer reveal for the next country
+    setModalVisible(false); // Hide the modal
   };
 
   const handleKeyPress = (e) => {
@@ -46,6 +55,7 @@ function App() {
 
   return (
     <div className='container'>
+      <img src={logo} alt="Logo" className="logo" /> {/* Logo added here */}
       <div className='clues-container'>
         <div className='clues'>
           {Array(5).fill(0).map((_, index) => (
@@ -67,8 +77,16 @@ function App() {
         value={userGuess}
         onChange={(e) => setUserGuess(e.target.value)}
         onKeyDown={handleKeyPress}
+        placeholder='...'
       />
-      <button onClick={nextCountry}>Next Country</button>
+
+      <div className='countryToggle'>
+      <button onClick={nextCountry}>Previous</button>
+      <button onClick={nextCountry}>Next</button>
+      </div>
+
+     
+      
     </div>
   );
 }
